ddeaa3bdaafadfef7b6c100387d41471296cf71c
J <badcats01@gmail.com>
2020-08-27 17:26:11 +0900

    Update __init__.py

--- angr_platforms/risc_v/__init__.py
+++ angr_platforms/risc_v/__init__.py
@@ -1,4 +1,4 @@
-from .arch_riscv import ArchRISCV
-from .lift_riscv import LifterRISCV32
+from .arch_riscv import ArchRISCV64
+from .lift_riscv import LifterRISCV64
 from .simos_riscv import SimCCRISCV, SimRISCVSyscall
 
