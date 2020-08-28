616380b52f033af80c989d31d9286c6efe53c9b8
J <badcats01@gmail.com>
2020-08-28 11:34:42 +0900

    Update instrs_riscv.py

--- angr_platforms/risc_v/instrs_riscv.py
+++ angr_platforms/risc_v/instrs_riscv.py
@@ -2,7 +2,7 @@ import abc
 from pyvex.lifting.util import Instruction, JumpKind, ParseError, Type
 import bitstring
 from bitstring import Bits, BitArray
-from .simos_riscv import SimRISCVSyscall
+from .simos_riscv import SimRISCV64Syscall
 import logging
 l = logging.getLogger(__name__)
 
