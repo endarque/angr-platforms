e7b1344f03a723bb94f9a88f356d5e7aed2b321a
J <badcats01@gmail.com>
2020-08-28 11:34:17 +0900

    Update simos_riscv.py

--- angr_platforms/risc_v/simos_riscv.py
+++ angr_platforms/risc_v/simos_riscv.py
@@ -1,10 +1,10 @@
 from angr.simos import SimOS, register_simos
 from angr.sim_procedure import SimProcedure
 from angr.calling_conventions import SimStackArg, SimRegArg, SimCC, register_syscall_cc, register_default_cc, SimCC
-from .arch_riscv import ArchRISCV
+from .arch_riscv import ArchRISCV64
 
 
-class SimCCRISCV(SimCC):
+class SimCCRISCV64(SimCC):
     ARG_REGS = [ 'a0', 'a1', 'a2', 'a3', 'a4', 'a5']
     FP_ARG_REGS = []    # expand in case the floating point extension is added
     STACK_ALIGNMENT = 16
@@ -12,7 +12,7 @@ class SimCCRISCV(SimCC):
     RETURN_VAL = SimRegArg('ra', 4)
     ARCH = ArchRISCV
 
-class SimRISCVSyscall(SimCC):
+class SimRISCV64Syscall(SimCC):
     ARG_REGS = ['a0', 'a1', 'a2', 'a3', 'a4', 'a5']
     RETURN_VAL = SimRegArg('a0', 4)
     RETURN_ADDR = SimStackArg(4, 4)
@@ -27,5 +27,5 @@ class SimRISCVSyscall(SimCC):
     def syscall_num(state):
         return state.regs.a7
 
-register_syscall_cc('RISCV', 'Linux', SimRISCVSyscall)
-register_default_cc('RISCV', SimCCRISCV)
+register_syscall_cc('RISCV', 'Linux', SimRISCV64Syscall)
+register_default_cc('RISCV', SimCCRISCV64)
